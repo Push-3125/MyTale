a3fb08b6ee596b4bc4c35f1cb5c40dca4aa15edd
StyxHavenVN <36104319+StyxHavenVN@users.noreply.github.com>
2025-09-26 22:07:08 +0700

    add reading-library
    include general design

---
+++ frontend/src/lib/sieu-nang-lap-phuong.ts
@@ -0,0 +1,74 @@
+import { Manga } from './manga-data';
+
+export const sieuNangLapPhuong: Manga = {
+  id: 'sieu-nang-lap-phuong',
+  title: 'Siêu Năng Lập Phương',
+  description: 'Truyện Siêu Năng Lập Phương cập nhật mới nhất và sớm nhất tại góc truyện tranh.',
+  coverImage: '/manga/sieu-nang-lap-phuong/cover.jpg',
+  coverImageHint: 'manhua, siêu năng lực',
+  chapters: [
+    {
+      id: 'chapter-313',
+      title: 'Chương 313',
+      chapterNumber: 313,
+      pages: [
+        {
+          id: 'p1',
+          pageNumber: 1,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/1.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p2',
+          pageNumber: 2,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/2.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p3',
+          pageNumber: 3,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/3.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p4',
+          pageNumber: 4,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/4.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p5',
+          pageNumber: 5,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/5.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p6',
+          pageNumber: 6,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/6.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p7',
+          pageNumber: 7,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/7.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+        {
+          id: 'p8',
+          pageNumber: 8,
+          imageUrl: '/manga/sieu-nang-lap-phuong/313/8.jpg',
+          imageHint: 'manhua page',
+          text: '',
+        },
+      ],
+    },
+  ],
+};
